70657a22f0f251298cb002e9caeb8e56e8b479d3
Justin Barton <bartonjustin@gmail.com>
2014-11-01 00:32:31 +1300

    first use of async and transducer

--- src/transerver/core.clj
+++ src/transerver/core.clj
@@ -1,13 +1,19 @@
 (ns transerver.core
   (:import java.net.InetSocketAddress
            java.nio.ByteBuffer
-           [java.nio.channels SelectionKey Selector ServerSocketChannel SocketChannel]))
+           [java.nio.channels SelectionKey Selector ServerSocketChannel SocketChannel])
+  (:require [clojure.core.async :refer [chan <! >! go dropping-buffer]]))
+
+(def xlog (map (fn [b] (println "xlog" (String. b)) b)))
+(def xxlog (map (fn [b] (println "xxlog" (String. b)) b)))
 
 (defn -main [& args]
   (println "Hello, World!")
   (let [selector (Selector/open)
         server (doto (ServerSocketChannel/open) (.configureBlocking false))
-        buffer (ByteBuffer/allocate 2048)]
+        buffer (ByteBuffer/allocate 2048)
+        reqchan (chan (dropping-buffer 10) (comp xlog xxlog))]
+
         (.bind (.socket server) (InetSocketAddress. "localhost" 5555))
         (.register server selector SelectionKey/OP_ACCEPT)
         (loop []
@@ -27,6 +33,7 @@
                          (let [sock (.channel k)
                                n (.read sock buffer)]
                             (println "read " n " bytes")
+                            (go (>! reqchan (.array buffer)))
                             (if (neg? n)
                                 (do (.close sock)
                                     (.cancel k))
@@ -37,7 +44,8 @@
                                buf (ByteBuffer/wrap (.array buffer))]
                           (loop []
                             (.write sock buf)
-                            (if (pos? (.remaining buf)) (recur) (.interestOps k SelectionKey/OP_READ)))
-                         ))
+                            (if-not (pos? (.remaining buf))
+                               (.interestOps k SelectionKey/OP_READ)
+                               (recur)))))
                     :else nil)))
                     (recur))))
